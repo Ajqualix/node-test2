1ebca05ed8176269f06c3bc9403ba54a314e5777
yucai <ajqualix@gmail.com>
2018-07-27 21:19:45 -0400

    start and port

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/',(req, res)=>{
     res.send('test');
 
 });
 
-app.listen(3000,()=>{
-    console.log('listening to port 3000');
+app.listen(port,()=>{
+    console.log(`listening to port ${port}`);
 })
